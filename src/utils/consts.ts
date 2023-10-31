6e55c5fe5a96eba78a7228d430d3e8579744f8d3
David Lee <dvdlee2454@gmail.com>
2023-10-31 22:14:25 +0100

    feat: add assets files

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -2,6 +2,11 @@ import Logo from 'assets/img/logo-blk.png'
 import BgLeft from 'assets/img/left-img.png'
 import BgBlur from 'assets/img/bg-blur.png'
 import BgCenter from 'assets/img/img-center.png'
+import SmallLogo from 'assets/img/small-logo-blk.png'
+import Avatar from 'assets/img/avatar.jpg'
+import TBanner1 from 'assets/img/ticket-banner1.jpg'
+import TBanner2 from 'assets/img/ticket-banner2.jpg'
+import ProfileBanner from 'assets/img/profile-banner.jpg'
 
 export const PATHS = {
   LOGIN: '/login',
@@ -14,7 +19,12 @@ export const PATHS = {
 
 export const IMGS = {
   LOGO: Logo,
+  SLOGO: SmallLogo,
   BGLEFT: BgLeft,
   BGBLUR: BgBlur,
   BGCENTER: BgCenter,
+  AVATAR: Avatar,
+  TBANNER1: TBanner1,
+  TBANNER2: TBanner2,
+  PBANNER: ProfileBanner,
 }
