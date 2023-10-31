901860000b1c3d5f90b65ed891d3bdba9f099743
David Lee <dvdlee2454@gmail.com>
2023-10-31 21:55:06 +0100

    feat: add new icon components

---
+++ src/components/common/icons/plus.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const PlusIconComponent: React.FC<
+  React.SVGProps<SVGSVGElement>
+> = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <rect width="24" height="24" rx="7" fill="#EBECFB" />
+      <rect x="11" y="7" width="2" height="10" fill="#6772FE" />
+      <rect
+        x="7"
+        y="13"
+        width="2"
+        height="10"
+        transform="rotate(-90 7 13)"
+        fill="#6772FE"
+      />
+    </svg>
+  )
+}
